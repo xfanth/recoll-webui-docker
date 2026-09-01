0c8f2130c93c08680567205e3d5a38fe135b1a80
Dev User <dev@example.com>
2026-09-01 04:10:46 +0000

    Fix CI: Update Python 2 syntax in bottle.py, fix webui.py filter() for Python 3, add yaml dependency

--- recoll-webui/webui.py
+++ recoll-webui/webui.py
@@ -9,8 +9,8 @@ import glob
 import hashlib
 import json
 import csv
-import StringIO
-import ConfigParser
+import io as StringIO
+import configparser as ConfigParser
 import string
 import shlex
 import urllib
@@ -145,7 +145,7 @@ def get_dirs(tops, depth):
         dirs = [top]
         for d in range(1, depth+1):
             dirs = dirs + glob.glob(top + '/*' * d)
-        dirs = filter(lambda f: os.path.isdir(f), dirs)
+        dirs = list(filter(lambda f: os.path.isdir(f), dirs))
         v = v + dirs
     return ['<all>'] + v
 #}}}
@@ -343,7 +343,7 @@ def get_csv():
     bottle.response.headers['Content-Type'] = 'text/csv'
     bottle.response.headers['Content-Disposition'] = 'attachment; filename=recoll-%s.csv' % normalise_filename(qs)
     res, nres, timer = recoll_search(query, False)
-    si = StringIO.StringIO()
+    si = io.StringIO()
     cw = csv.writer(si)
     fields = config['csvfields'].split()
     cw.writerow(fields)
